706b2e31504f5bd5ae87df1f7f5f81cfee268af6
shiks25 <56836929+shiks25@users.noreply.github.com>
2019-11-21 23:22:55 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,104 @@
+import java.util.*;
+class Account
+{
+  String cust_name,acc_type;
+  long  acc_no;
+  double bal=0.0;
+ void input()
+{
+  System.out.println("Enter the cst name,acc type and acc no.");
+Scanner sc =new Scanner(System.in);
+cust_name=sc.next();
+acc_type=sc.next();
+acc_no = sc.nextInt();
+}
+void display()
+{
+ System.out.println("Balance="+bal);
+}
+void deposit()
+{
+ System.out.println("enter the amount to be deposited");
+ double deposit=sc.nextDouble();
+bal+=deposit;
+}
+void withdraw()
+{
+System.out.println("enter the amount to be withdrawn");
+double draw=sc.nextDouble();
+bal-=draw;
+}
+abstract void interestCal();
+abstract void service();
+}
+class sav_acct extends Account
+{
+final r=5;
+ int t;
+double interest;
+Scanner sc=new Scanner(System.in);
+void input()
+{
+ super.input();
+System.out.println("enter the time");
+t=sc.nextInt();
+}
+void interestCall()
+{
+ interest= bal*(Math.pow(1.05,t));      // 1+r*0.01 and r=5
+bal=bal+interest;
+}
+void display()
+{
+ System.out.println("Interest="+interest);
+super.display();
+}
+}
+class curr_acc extends Account
+{
+Scanner s=new Scanner(System.in);
+ final double min=1000.00;
+final double ser=800.00;
+void input()
+{
+super.input();
+}
+void service()
+{
+ if(bal<min)
+   bal=bal-ser;
+}
+void display()
+{
+ super.display();
+}
+}
+class Bank
+{
+ public static void main(String args[]) {
+  Scanner s=new Scanner(System.in);
+System.out.println("Enter 1 for savings 2. for current account");
+int ch=sc.nextInt();
+Account ref;
+if(ch=1)
+{
+ ref=new Sav_acc;
+}
+else
+{
+ref=new Curr_acc;
+}
+ref.input();
+ref.deposit();
+ref.withdraw();
+ref.interestCal();
+ref.service();
+ref.display();
+}
+}
+
+
+  
+
+ 
+ 
