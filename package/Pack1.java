8a40422c020780bdde1ebd9eb139d7b2edb832af
Shikha25 <56836929+shiks25@users.noreply.github.com>
2019-11-28 21:37:36 +0530

    Update Pack1.java

--- package/Pack1.java
+++ package/Pack1.java
@@ -12,14 +12,18 @@ import java.util.*;
   System.out.println("Enter the number of students");
   Scanner s= new Scanner(System.in);
   n=s.nextInt();
+    Internals a[]=new Internals[n];
+   Externals b[]=new Externals[n];
    for(int i=0;i<n;i++){
-   Internals a=new Internals();
-   Externals b=new Externals();
    System.out.println("for student"+(i+1));
+   a[i]=new Internals();
+   b[i]=new Externals();
    a.get_details();
    a.get_cie();
    b.get_see();
-        a.display_details();
+   a.display_details();
+   a.display_cie();
+   b.display_see();
    	for(int j=0;j<5;j++){
  	System.out.println("final marks of course"+(j+1));
  	System.out.println(a.marks_cie[j]+b.marks_see[j]);
